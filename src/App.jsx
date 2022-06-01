682086b70408153b77f9fbd1e76c2c058625608e
saidugurlu <ugurlumsaid@gmx.de>
2022-06-01 10:04:23 +0200

    buttons are dynamically colors with styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,22 @@ import { useEffect, useState } from 'react';
 import './App.scss';
 import axios from 'axios';
 import * as qstr from './qstr';
+import styled from 'styled-components';
 
 const url = 'https://edwardtanguay.netlify.app/share/germanNouns.json';
 
 function App() {
 	const [nouns, setNouns] = useState([]);
-  const [choice, setChoice] = useState('hide');
+	const [choice, setChoice] = useState('hide');
+
+  const Button = styled.button`
+      background-color: ${(props) =>  props.mode === 'selected' ? 'green' : '#eee'}; 
+  `;
 
 	useEffect(() => {
 		(async () => {
-      const _nouns = (await axios.get(url)).data;
-      qstr.randomize(_nouns);
+			const _nouns = (await axios.get(url)).data;
+			qstr.randomize(_nouns);
 			setNouns(_nouns);
 		})();
 	}, []);
@@ -20,19 +25,24 @@ function App() {
 	return (
 		<div className="App">
 			<h1>German Article Practice</h1>
-      <div className="buttons">
-        <button className={choice === 'der' ? 'active' : ''} onClick={() => setChoice('der')}>der</button>
-        <button onClick={() => setChoice('die')}>die</button>
-        <button onClick={() => setChoice('das')}>das</button>
-        <button onClick={() => setChoice('hide')}>hide</button>
-      </div>
-       <div className="nouns">
-        {nouns.map((noun, index) => {
-          return (
-            <div className="noun">{noun.article} {noun.singular}</div>
-          )
-        })}
-      </div>
+			<div className="buttons">
+        <Button mode={choice === 'der' ? 'selected' : 'unselected'} onClick={() => setChoice('der')}>der</Button>
+
+				<Button mode={choice === 'die' ? 'selected' : 'unselected'}  onClick={() => setChoice('die')}>die</Button>
+
+				<Button mode={choice === 'das' ? 'selected' : 'unselected'} onClick={() => setChoice('das')}>das</Button>
+
+				<Button mode={choice === 'hide' ? 'selected' : 'unselected'} onClick={() => setChoice('hide')}>hide</Button>
+			</div>
+			<div className="nouns">
+				{nouns.map((noun, index) => {
+					return (
+						<div className="noun">
+							{noun.article} {noun.singular}
+						</div>
+					);
+				})}
+			</div>
 		</div>
 	);
 }
